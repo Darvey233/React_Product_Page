6e99e13ce381cba98d891ff697efe7b26d57d4db
Darvey233 <davidqiu1997@hotmail.com>
2021-07-03 21:57:33 +1200

    update expendable menu in mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,23 +6,32 @@ import { Link } from 'react-router-dom'
 import './css/Header.css'
 
 export class Header extends Component {
+
+  state = {
+    toggle: false
+  }
+
+  menuToggle = () =>{
+    this.setState({toggle: !this.state.toggle})
+  }
+
   render(){
-    
+    const {toggle} = this.state;
     return (
       <header>
-        <div className="menu">
+        <div className="menu" onClick={this.menuToggle}>
           <img src={Menu} alt="" width="20"/>
         </div>
         <div className="logo">
           <h1><Link to="/">Audi</Link></h1>
         </div>
         <nav>
-          <ul>
+          <ul className={toggle ? "toggle" : ""}>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/">Vehicle</Link></li>
-            <li><Link to="/">Contact</Link></li>
-            <li><Link to="/">About</Link></li>
-            <li className="close">
+            <li><Link to="/vehicle">Vehicle</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li className="close" onClick={this.menuToggle}>
               <img src={Close} alt="" width="20"/>
             </li>
           </ul>
