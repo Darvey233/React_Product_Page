ac7da9f5662ad3112ef4fd4c06f953b1ec5f0883
David Qiu <davidqiu1997@hotmail.com>
2021-07-23 18:38:20 +1200

    Testing folder name change

--- src/components/Section/Products.js
+++ src/components/Section/Products.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import {DataContext} from '../Context'
 
 export class Products extends Component {
+
+  static contextType = DataContext;
+
   render(){
+    const {products} = this.context;
     
     return (
-      <div>
-        Products
+      <div id="product">
+        {
+          products.map(product =>(
+
+            <div className="card" key={product._id}>
+              <Link to={`/product/${product._id}`}>
+                <img src={product.src} alt=""/>
+              </Link>
+            </div>
+          ))
+        }
+        
       </div>
     )
   }
